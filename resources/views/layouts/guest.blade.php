bd43a6c859d9900c452918029602af01c2f5593a
Slof <slof@hey.com>
2023-01-17 11:26:08 -0600

    Update guest.blade.php

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -25,14 +25,18 @@
 </head>
 
 <body class="font-sans text-zinc-900 antialiased">
-    <div class="min-h-screen flex flex-col sm:justify-center items-center pt-6 sm:pt-0 bg-zinc-100 dark:bg-zinc-800">
+    <div class="min-h-screen flex flex-col sm:justify-center items-center pt-6 sm:pt-0 bg-zinc-900">
         <div class="flex flex-col items-center space-y-2">
-            <a href="/">
+            <a
+                href="/"
+                class="focusable rounded-md"
+            >
                 {{-- <x-application-logo class="w-20 h-20 fill-current text-zinc-500" /> --}}
-                <p class="text-zinc-800 dark:text-zinc-50 text-3xl font-semibold">code<span
-                        class="text-sky-400">snips</span></p>
+                <p class="text-zinc-50 text-3xl font-semibold">
+                    code<span class="text-cyan-400">snips</span>
+                </p>
             </a>
-            <p class="text-zinc-600 dark:text-zinc-200">Self-hoted S3 compatible code snippet storage</p>
+            <p class="text-zinc-200">Self-hosted S3 compatible code snippet storage</p>
         </div>
 
         <div class="w-full sm:max-w-md mt-10">
