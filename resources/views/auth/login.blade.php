47ee792e6b31bafb7898e9bfa3579a56ec039b1f
Slof <slof@hey.com>
2023-01-17 10:03:51 -0600

    Refactor Login page to use new components

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -13,15 +13,18 @@
 
         <!-- Email Address -->
         <div>
-            <x-input-label
+            <x-inputs.label
                 for="email"
-                :value="__('Email')"
-            />
-            <x-text-input
+                class="w-full block"
+            >
+                {{ __('Email') }}
+            </x-inputs.label>
+            <x-inputs.text
                 id="email"
                 class="block mt-1 w-full"
                 type="email"
                 name="email"
+                placeholder="Enter your email address"
                 :value="old('email')"
                 required
                 autofocus
@@ -34,16 +37,19 @@
 
         <!-- Password -->
         <div class="mt-4">
-            <x-input-label
+            <x-inputs.label
                 for="password"
-                :value="__('Password')"
-            />
+                class="block w-full"
+            >
+                {{ __('Password') }}
+            </x-inputs.label>
 
-            <x-text-input
+            <x-inputs.text
                 id="password"
                 class="block mt-1 w-full"
                 type="password"
                 name="password"
+                placeholder="Enter your password"
                 required
                 autocomplete="current-password"
             />
@@ -55,41 +61,53 @@
         </div>
 
         <div class="flex items-center justify-between mt-8">
-            <label
-                for="remember_me"
-                class="inline-flex items-center"
-            >
-                <input
-                    id="remember_me"
-                    type="checkbox"
-                    class="rounded dark:bg-zinc-900 border-zinc-300 dark:border-zinc-700 text-sky-600 shadow-sm focus:ring-sky-500 dark:focus:ring-sky-600 dark:focus:ring-offset-zinc-800"
+            <x-inputs.checkbox-group for="remember_me">
+                <x-inputs.checkbox
                     name="remember"
-                >
-                <span class="ml-2 text-sm text-zinc-600 dark:text-zinc-400">{{ __('Remember me') }}</span>
-            </label>
-            <x-primary-button class="ml-3">
-                {{ __('Log in') }}
-            </x-primary-button>
+                    id="remember_me"
+                />
+
+                <x-inputs.checkbox-text class="ml-2">
+                    Remember Me
+                    <span
+                        class="ml-2"
+                        title="Selecting this option will keep you signed in on this device indefinitely until you sign out on your own. It is recommended to only use this on personal devices."
+                    >
+                        <svg
+                            xmlns="http://www.w3.org/2000/svg"
+                            viewBox="0 0 20 20"
+                            fill="currentColor"
+                            class="w-5 h-5"
+                        >
+                            <path
+                                fill-rule="evenodd"
+                                d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-7-4a1 1 0 11-2 0 1 1 0 012 0zM9 9a.75.75 0 000 1.5h.253a.25.25 0 01.244.304l-.459 2.066A1.75 1.75 0 0010.747 15H11a.75.75 0 000-1.5h-.253a.25.25 0 01-.244-.304l.459-2.066A1.75 1.75 0 009.253 9H9z"
+                                clip-rule="evenodd"
+                            />
+                        </svg>
+                    </span>
+                </x-inputs.checkbox-text>
+            </x-inputs.checkbox-group>
+
+            <div class="flex">
+                <x-buttons.primary type="submit">
+                    Sign In
+                </x-buttons.primary>
+            </div>
         </div>
 
         <div class="h-[1px] w-full bg-zinc-600 my-10"></div>
 
         <div class="flex flex-col space-y-4 items-center">
             @if (Route::has('password.request'))
-                <a
-                    class="underline text-sm text-zinc-600 dark:text-zinc-400 hover:text-zinc-900 dark:hover:text-zinc-100 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-sky-500 dark:focus:ring-offset-zinc-800"
-                    href="{{ route('password.request') }}"
-                >
+                <x-links.anchor href="{{ route('password.request') }}">
                     {{ __('Forgot your password?') }}
-                </a>
+                </x-links.anchor>
             @endif
 
-            <a
-                class="underline text-sm text-zinc-600 dark:text-zinc-400 hover:text-zinc-900 dark:hover:text-zinc-100 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-sky-500 dark:focus:ring-offset-zinc-800"
-                href="{{ route('register') }}"
-            >
+            <x-links.anchor href="{{ route('register') }}">
                 {{ __('Don\'t have an account?') }}
-            </a>
+            </x-links.anchor>
         </div>
     </form>
 </x-guest-layout>
