dac84c27783d84be155fbdcc5d7975d455aa42b8
Slof <slof@hey.com>
2023-01-25 19:32:22 -0600

    Remove main site nav links

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -1,10 +1,7 @@
-<nav
-    x-data="{ open: false }"
-    class="bg-white dark:bg-zinc-800 border-b border-zinc-100 dark:border-zinc-700"
->
+<nav x-data="{ open: false }">
     <!-- Primary Navigation Menu -->
     <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
-        <div class="flex justify-between h-16">
+        <div class="flex justify-between py-6">
             <div class="flex">
                 <!-- Logo -->
                 <div class="shrink-0 flex items-center">
@@ -14,29 +11,31 @@
                 </div>
 
                 <!-- Navigation Links -->
-                <div class="hidden space-x-8 sm:-my-px sm:ml-10 sm:flex">
+                {{-- <div class="hidden space-x-8 sm:-my-px sm:ml-10 sm:flex">
                     <x-nav-link
                         :href="route('dashboard')"
                         :active="request()->routeIs('dashboard')"
                     >
                         {{ __('Dashboard') }}
                     </x-nav-link>
-                </div>
+                </div> --}}
             </div>
 
             <!-- Settings Dropdown -->
-            <div class="hidden sm:flex sm:items-center sm:ml-6">
+            <div class="flex items-center">
                 <x-dropdown
                     align="right"
                     width="48"
                 >
                     <x-slot name="trigger">
-                        <button
-                            class="inline-flex items-center px-3 py-2 border border-transparent text-sm leading-4 font-medium rounded-md text-zinc-500 bg-white dark:bg-zinc-800 hover:text-zinc-700 dark:hover:text-zinc-300 focus:outline-none transition ease-in-out duration-150"
-                        >
-                            <div>{{ Auth::user()->username }}</div>
 
-                            <div class="ml-1">
+                        <button
+                            type="button"
+                            class="inline-flex items-center justify-between font-semibold transition ease-in-out duration-150 focusable text-md px-2 py-1 rounded-md text-zinc-50 bg-black/20 border-2 border-zinc-600 hover:bg-black/40 hover:border-zinc-500 active:bg-black/60 focus:bg-black/60 active:border-zinc-500 focus:border-zinc-500 min-w-[120px]"
+                        >
+                            <div>{{ '@' }}{{ Auth::user()->username }}</div>
+
+                            <div class="ml-2">
                                 <svg
                                     class="fill-current h-4 w-4"
                                     xmlns="http://www.w3.org/2000/svg"
@@ -50,6 +49,11 @@
                                 </svg>
                             </div>
                         </button>
+                        {{-- <button
+                            class="inline-flex items-center px-3 py-2 border border-transparent text-sm leading-4 font-medium rounded-md text-zinc-500 bg-white dark:bg-zinc-800 hover:text-zinc-700 dark:hover:text-zinc-300 focus:outline-none transition ease-in-out duration-150"
+                        >
+
+                        </button> --}}
                     </x-slot>
 
                     <x-slot name="content">
@@ -77,7 +81,7 @@
             </div>
 
             <!-- Hamburger -->
-            <div class="-mr-2 flex items-center sm:hidden">
+            {{-- <div class="-mr-2 flex items-center sm:hidden">
                 <button
                     @click="open = ! open"
                     class="inline-flex items-center justify-center p-2 rounded-md text-zinc-400 dark:text-zinc-500 hover:text-zinc-500 dark:hover:text-zinc-400 hover:bg-zinc-100 dark:hover:bg-zinc-900 focus:outline-none focus:bg-zinc-100 dark:focus:bg-zinc-900 focus:text-zinc-500 dark:focus:text-zinc-400 transition duration-150 ease-in-out"
@@ -106,12 +110,12 @@
                         />
                     </svg>
                 </button>
-            </div>
+            </div> --}}
         </div>
     </div>
 
     <!-- Responsive Navigation Menu -->
-    <div
+    {{-- <div
         :class="{ 'block': open, 'hidden': !open }"
         class="hidden sm:hidden"
     >
@@ -153,5 +157,5 @@
                 </form>
             </div>
         </div>
-    </div>
+    </div> --}}
 </nav>
